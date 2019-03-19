096f102a3f03d023affd96c8d928892a8d583899
Jose Manuel Estrada-Nora Muñoz <jotajotanora@gmail.com>
2019-03-19 12:00:59 +0100

    SDI-NodeJS- 6,7 División de las rutas en módulos, peticiones Get y parámetros

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ var app = express();
 // Variables
 app.set('port', 8081);
 
-app.get('/usuarios', function (req, res) {
-    console.log("Depurar aquó");
-    res.send('ver usuarios');
-});
-app.get('/canciones', function (req, res) {
-    res.send('ver canciones');
-});
+require("./routes/rusuarios.js")(app); // Se utilizan como funciones
+require("./routes/rcanciones.js")(app);
+
 // lanzar el servidor
 app.listen(app.get('port'), function () {
     console.log("Servidor activo");
