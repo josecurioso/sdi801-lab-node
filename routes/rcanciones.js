c998ec6c971e047e157a5a234a5494a4703cd9af
Jose Manuel Estrada-Nora Muñoz <jotajotanora@gmail.com>
2019-03-26 12:05:30 +0100

    SDI-NodeJS-8.4 - Subida de ficheros y gestion de colecciones

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -39,7 +39,7 @@ module.exports = function (app, swig, gestorBD) {
     });
     app.post("/cancion", function(req, res) {
         var cancion = {
-            nombre: req.body.nmbre,
+            nombre: req.body.nombre,
             genero: req.body.genero,
             precio: req.body.precio
         };
@@ -48,11 +48,64 @@ module.exports = function (app, swig, gestorBD) {
             if (id == null) {
                 res.send("Error al insertar canción");
             } else {
-                res.send("Agregada la canción ID: " + id);
+                if (req.files.portada != null) {
+                    var imagen = req.files.portada;
+                    imagen.mv('public/portadas/' + id + '.png', function(err) {
+                        if (err) {
+                            res.send("Error al subir la portada");
+                        } else {
+                            if (req.files.audio != null) {
+                                var audio = req.files.audio;
+                                audio.mv('public/audios/'+id+'.mp3', function(err) {
+                                    if (err) {
+                                        res.send("Error al subir el audio");
+                                    } else {
+                                        res.send("Agregada id: "+ id);
+                                    }
+                                });
+                            }
+
+                        }
+                    });
+                }
             }
         });
     });
     app.get('/promo*', function (req, res) {
         res.send('Respuesta patrón promo* ');
     });
+
+    app.get('/tienda', function(req, res) {
+        var criterio = {};
+        if( req.query.busqueda != null ){
+            criterio = {"nombre" : {$regex : ".*"+req.query.busqueda+".*"} };
+        }
+        gestorBD.obtenerCanciones(criterio,function(canciones) {
+            if (canciones == null) {
+                res.send("Error al listar ");
+            } else {
+                var respuesta = swig.renderFile('views/btienda.html',
+                    {
+                        canciones : canciones
+                    });
+                res.send(respuesta);
+            }
+        });
+
+    })
+
+    app.get('/cancion/:id', function (req, res) {
+        var criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)  };
+        gestorBD.obtenerCanciones(criterio,function(canciones){
+            if ( canciones == null ){
+                res.send(respuesta);
+            } else {
+                var respuesta = swig.renderFile('views/bcancion.html',
+                    {
+                        cancion : canciones[0]
+                    });
+                res.send(respuesta);
+            }
+        });
+    })
 };
